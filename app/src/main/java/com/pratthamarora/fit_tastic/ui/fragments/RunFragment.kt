eb9f299023322c05c1b0782a7217c293ef05893d
PratthamArora <arora.prattham@gmail.com>
2020-06-21 21:43:22 +0530

    added layouts

---
+++ app/src/main/java/com/pratthamarora/fit_tastic/ui/fragments/RunFragment.kt
@@ -0,0 +1,7 @@
+package com.pratthamarora.fit_tastic.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.pratthamarora.fit_tastic.R
+
+class RunFragment:Fragment(R.layout.fragment_run) {
+}
