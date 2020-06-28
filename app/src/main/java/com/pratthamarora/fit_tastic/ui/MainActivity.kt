008898446f2b1a8d243e4621b38da6a56214b678
PratthamArora <arora.prattham@gmail.com>
2020-06-28 17:12:43 +0530

    setup navigation using navcontroller

--- app/src/main/java/com/pratthamarora/fit_tastic/ui/MainActivity.kt
+++ app/src/main/java/com/pratthamarora/fit_tastic/ui/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.pratthamarora.fit_tastic.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.pratthamarora.fit_tastic.R
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        supportActionBar?.subtitle = "by - Prattham Arora"
+        supportActionBar?.subtitle = getString(R.string.dev_name)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController().addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.settingsFragment, R.id.runFragment, R.id.statsFragment -> bottomNavigationView.isVisible =
+                    true
+                else -> bottomNavigationView.isGone = true
+            }
+        }
     }
 }
--- app/src/main/java/com/pratthamarora/fit_tastic/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/pratthamarora/fit_tastic/ui/fragments/RunFragment.kt
@@ -1,13 +1,25 @@
 package com.pratthamarora.fit_tastic.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.pratthamarora.fit_tastic.R
 import com.pratthamarora.fit_tastic.ui.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
