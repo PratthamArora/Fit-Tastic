715e03aea88b15f9f7488288d1ed6114b12f844a
PratthamArora <arora.prattham@gmail.com>
2020-07-11 19:55:19 +0530

    update notification time

---
+++ app/src/main/java/com/pratthamarora/fit_tastic/di/ServiceModule.kt
@@ -0,0 +1,53 @@
+package com.pratthamarora.fit_tastic.di
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.pratthamarora.fit_tastic.R
+import com.pratthamarora.fit_tastic.ui.MainActivity
+import com.pratthamarora.fit_tastic.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideFusedLocationProviderClient(
+        @ApplicationContext app: Context
+    ) = FusedLocationProviderClient(app)
+
+    @ServiceScoped
+    @Provides
+    fun provideMainActivityPendingIntent(
+        @ApplicationContext app: Context
+    ) = PendingIntent.getActivity(
+        app,
+        0,
+        Intent(app, MainActivity::class.java).also {
+            it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    @ServiceScoped
+    @Provides
+    fun provideBaseNotificationBuilder(
+        @ApplicationContext app: Context,
+        pendingIntent: PendingIntent
+    ) = NotificationCompat.Builder(app, Constants.NOTIFICATION_CHANNEL_ID)
+        .setAutoCancel(false)
+        .setOngoing(true)
+        .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+        .setContentTitle("Current Run")
+        .setContentText("00:00:00")
+        .setContentIntent(pendingIntent)
+}
